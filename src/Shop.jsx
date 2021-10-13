5e0194286d2a6df56fbea5d0071a160c0b778557
itstudent17 <itstudent17@outlook.com>
2021-10-13 22:00:59 +0300

    fetching data form fortnite api

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,7 +1,25 @@
-const Shop = () => (
-  <div>
-    <h1>Shop Page</h1>
-  </div>
-);
+import { useState, useEffect } from "react";
+
+const Shop = () => {
+  const fetchItems = async () => {
+    const data = await fetch(
+      // "https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get"
+      "https://fortnite-api.theapinetwork.com/store/get"
+    );
+    const items = await data.json();
+    console.log(data, items);
+  };
+
+  useEffect(() => {
+    fetchItems();
+    return () => {};
+  }, []);
+
+  return (
+    <div>
+      <h1>Shop Page</h1>
+    </div>
+  );
+};
 
 export default Shop;
