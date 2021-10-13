d7bc9304d06b97d52e92206fdb3fded40994be02
itstudent17 <itstudent17@outlook.com>
2021-10-13 21:34:15 +0300

    Router, Switch, Route components used

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import About from "./About";
 import Navigation from "./Nav";
 import Shop from "./Shop";
 import "./App.css";
+import Home from "./Home";
 
 const App = () => {
   return (
-    <div className="App">
-      <Navigation />
-      <About />
-      <Shop />
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        {/* problem with / represented in all path routes */}
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/shop" component={Shop} />
+      </div>
+    </Router>
   );
 };
 
