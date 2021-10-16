4273bcedaafbc42bcca2b72508d6e3a806dde3f9
itstudent17 <itstudent17@outlook.com>
2021-10-16 21:34:48 +0300

    props.match for item page

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,3 +1,29 @@
-const Item = () => <h1>This is Item Page</h1>;
+import { useState, useEffect } from "react";
+
+const Item = (props) => {
+  const [item, setItem] = useState({});
+
+  const fetchItem = async () => {
+    // TODO fix error with API (url does not return an object - array instead)
+    const data = await fetch(
+      `https://fortnite-api.theapinetwork.com/store/get?id=${props.match.params.id}`
+    );
+    const item = await data.json();
+    console.log("fetched Item", item);
+    setItem(item);
+  };
+
+  useEffect(() => {
+    console.log("match from Route in App.js", props.match);
+    fetchItem();
+  }, []);
+
+  return (
+    <div>
+      <h1>{item.name}</h1>
+      <img src={item?.images?.transparent} alt="" />
+    </div>
+  );
+};
 
 export default Item;
