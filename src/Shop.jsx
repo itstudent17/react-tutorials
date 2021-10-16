a136c3a45e52dc6d4c482336feb830e559072cfe
itstudent17 <itstudent17@outlook.com>
2021-10-16 20:19:40 +0300

    receiving data from server

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,13 +1,17 @@
 import { useState, useEffect } from "react";
 
 const Shop = () => {
+  const [items, setItems] = useState([]);
+
   const fetchItems = async () => {
     const data = await fetch(
       // "https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get"
       "https://fortnite-api.theapinetwork.com/store/get"
     );
     const items = await data.json();
-    console.log(data, items);
+    console.log("data", data);
+    console.log("items", items.data);
+    setItems(items.data);
   };
 
   useEffect(() => {
@@ -18,6 +22,9 @@ const Shop = () => {
   return (
     <div>
       <h1>Shop Page</h1>
+      {items.map((i) => (
+        <h1 key={i.itemId}>{i.item.name}</h1>
+      ))}
     </div>
   );
 };
